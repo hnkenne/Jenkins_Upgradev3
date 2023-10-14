8f669151732f576b0f03207bb0b405752da56287
hnkenne <hnkenne@nrtechconsulting.com>
2023-10-14 16:35:17 -0500

    import lessthan

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -8,6 +8,7 @@ import org.mockito.internal.matchers.LessThan;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.*;
 import static org.junit.matchers.JUnitMatchers.containsString;
 
@@ -46,6 +47,6 @@ public class TestGreeter {
   public void greetShouldIncludeInspiringMessage() {
     String someone = "Hey you! Life is beautiful";
 
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+    assertThat(greeter.greet(someone).length(), is(lessThan(someone.length())));
   }
 }
